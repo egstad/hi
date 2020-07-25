a46fc1956b48d870aac7bf5a84d551983cb6e5ba
Egstad <jordanegstad@gmail.com>
2020-07-25 16:06:43 -0700

    added anon login

--- store/user.js
+++ store/user.js
@@ -8,11 +8,13 @@ export const state = () => ({
 export const mutations = {
   login(state, user) {
     state.isLoggedIn = true
+    state.isAnon = user.isAnonymous
     state.email = user.email
     state.uid = user.uid
   },
   logout(state) {
     state.isLoggedIn = false
+    state.isAnon = null
     state.email = null
     state.uid = null
   },
